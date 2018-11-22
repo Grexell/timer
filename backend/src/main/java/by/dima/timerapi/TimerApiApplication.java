5cd66fb6c0a092e4bea554fac76e5100546ef5f9
Grexell <demesh.dima.dd@gmail.com>
2018-11-22 22:09:39 +0300

    module separating

---
+++ backend/src/main/java/by/dima/timerapi/TimerApiApplication.java
@@ -0,0 +1,12 @@
+package by.dima.timerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimerApiApplication.class, args);
+    }
+}
---
+++ front-end/src/app/service/timer-service.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TimerServiceService {
+
+  constructor(private httpClient: ) { }
+
+  public get(id: int) {
+
+  }
+}
