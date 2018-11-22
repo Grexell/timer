6c66af3ba5c1b4049417c922f552b6b9c232675e
Grexell <demesh.dima.dd@gmail.com>
2018-11-22 21:51:39 +0300

    backend module separating

--- src/main/java/by/dima/timerapi/repository/TimerRepository.java
+++
@@ -1,7 +0,0 @@
-package by.dima.timerapi.repository;
-
-import by.dima.timerapi.model.Timer;
-import org.springframework.data.repository.CrudRepository;
-
-public interface TimerRepository extends CrudRepository<Timer, Integer> {
-}
--- src/main/java/by/dima/timerapi/service/TimerService.java
+++
@@ -1,8 +0,0 @@
-package by.dima.timerapi.service;
-
-import by.dima.timerapi.model.Timer;
-
-public interface TimerService {
-    Timer getById(int id);
-    int create(Timer timer);
-}
